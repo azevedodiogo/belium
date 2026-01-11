325b17171bfe52a11839b73b89e9af19ac7f1401
Diogo Azevedo <183098609+azevedodiogo@users.noreply.github.com>
2026-01-11 18:21:15 +0000

    feat: add file for user creation

---
+++ Create_Users.sql
@@ -0,0 +1,69 @@
+-- | Define a base de dados a ser utilizada
+USE BeLIUM_Viagens;
+
+
+
+-- | Cria o utilizador utilizador
+CREATE USER IF NOT EXISTS 'UTILIZADOR'@'localhost' IDENTIFIED BY 'BeLIUM_Viagens_UTILIZADOR_1025';
+
+-- | Cria o utilizador sócio
+CREATE USER IF NOT EXISTS 'SOCIO'@'localhost' IDENTIFIED BY 'BeLIUM_Viagens_SOCIO_2047';
+
+-- | Cria o utilizador administrador
+CREATE USER IF NOT EXISTS 'ADMIN'@'localhost' IDENTIFIED BY 'BeLIUM_Viagens_VDBS';
+
+
+
+-- | RC1. Para se registar, um utilizador deve pedir diretamente a um administrador ou enviar um pedido, por email. Se o pedido for aceite, um administrador deve registá-lo no sistema e comunicá-lo do seu ID e da sua password.
+GRANT INSERT ON BeLIUM_Viagens.Utilizador TO 'ADMIN'@'localhost';
+
+-- | RC2. Um utilizador pode consultar todos os dados dos sócios, das viagens e das paragens no sistema.
+GRANT SELECT ON BeLIUM_Viagens.Sócio TO 'UTILIZADOR'@'localhost';
+GRANT SELECT ON BeLIUM_Viagens.Viagem TO 'UTILIZADOR'@'localhost';
+GRANT SELECT ON BeLIUM_Viagens.Paragem TO 'UTILIZADOR'@'localhost';
+GRANT SELECT ON BeLIUM_Viagens.Deslocamento TO 'UTILIZADOR'@'localhost';
+GRANT SELECT ON BeLIUM_Viagens.Foto TO 'UTILIZADOR'@'localhost';
+
+-- | RC3. Um utilizador pode atualizar a sua password, se souber a atual.
+-- GRANT EXECUTE ON mudar_password TO 'UTILIZADOR'@'localhost';
+
+-- | RC5. Utilizadores podem visualizar e reagir às viagens. Podem, também, atualizar ou remover a visualização.
+-- GRANT EXECUTE ON inserir_visualização TO 'UTILIZADOR'@'localhost';
+-- GRANT EXECUTE ON atualizar_visualização TO 'UTILIZADOR'@'localhost';
+-- GRANT EXECUTE ON remover_visualização TO 'UTILIZADOR'@'localhost';
+
+-- | RC6. Os sócios possuem todas as permissões dos utilizadores.
+GRANT SELECT ON BeLIUM_Viagens.Sócio TO 'SOCIO'@'localhost';
+GRANT SELECT ON BeLIUM_Viagens.Viagem TO 'SOCIO'@'localhost';
+GRANT SELECT ON BeLIUM_Viagens.Paragem TO 'SOCIO'@'localhost';
+GRANT SELECT ON BeLIUM_Viagens.Deslocamento TO 'SOCIO'@'localhost';
+GRANT SELECT ON BeLIUM_Viagens.Foto TO 'SOCIO'@'localhost';
+-- GRANT EXECUTE ON mudar_password TO 'SOCIO'@'localhost';
+-- GRANT EXECUTE ON inserir_visualização TO 'SOCIO'@'localhost';
+-- GRANT EXECUTE ON atualizar_visualização TO 'SOCIO'@'localhost';
+-- GRANT EXECUTE ON remover_visualização TO 'SOCIO'@'localhost';
+
+-- | RC7. Os sócios podem inserir novas viagens, assim como as suas paragens. Podem, também, editá-las ou removê-las, caso necessário.
+GRANT INSERT, UPDATE, DELETE ON BeLIUM_Viagens.Viagem TO 'SOCIO'@'localhost';
+GRANT INSERT, UPDATE, DELETE ON BeLIUM_Viagens.Paragem TO 'SOCIO'@'localhost';
+GRANT INSERT, UPDATE, DELETE ON BeLIUM_Viagens.Deslocamento TO 'SOCIO'@'localhost';
+GRANT INSERT, UPDATE, DELETE ON BeLIUM_Viagens.Foto TO 'SOCIO'@'localhost';
+
+-- | RC8. Os sócios podem consultar todos os dados no sistema, à excessão dos dados pessoais dos utilizadores.
+GRANT SELECT ON BeLIUM_Viagens.Rel_Utilizador_Viagem TO 'SOCIO'@'localhost';
+GRANT SELECT ON BeLIUM_Viagens.Rel_Sócio_Viagem TO 'SOCIO'@'localhost';
+GRANT SELECT ON BeLIUM_Viagens.Rel_Viagem_Patrocinador TO 'SOCIO'@'localhost';
+GRANT SELECT ON BeLIUM_Viagens.Patrocinador TO 'SOCIO'@'localhost';
+GRANT SELECT ON BeLIUM_Viagens.Motivo TO 'SOCIO'@'localhost';
+
+-- | RC9. Um sócio pode publicar uma viagem com os respetivos avaliação e comentário. Pode, ainda, editar ou eliminar esta publicação.
+GRANT INSERT, UPDATE, DELETE ON BeLIUM_Viagens.Rel_Sócio_Viagem TO 'SOCIO'@'localhost';
+
+-- | RC10. Os administradores (membros do CCP) podem realizar qualquer tipo de operação no sistema, incluindo alterações nucleares, caso necessário.
+-- | RC11. Eventualmente, se necessário, sócios de confiança podem obter o estatudo de administrador do sistema (permissão atribuída por outros administradores).
+GRANT ALL PRIVILEGES ON BeLIUM_Viagens.* TO 'ADMIN'@'localhost' WITH GRANT OPTION;
+
+
+
+-- | Reseta os privilégios
+FLUSH PRIVILEGES;
